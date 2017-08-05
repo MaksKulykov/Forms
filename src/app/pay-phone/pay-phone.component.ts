c24b5f5342b95f108c8670c07bb896b731f9535c
MaksKulikov <kulykov.maks@gmail.com>
2017-08-05 12:09:06 +0300

    implemented OnInit in Pay component

--- src/app/pay-phone/pay-phone.component.ts
+++ src/app/pay-phone/pay-phone.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, InjectionToken} from '@angular/core';
+import {Component, Inject, InjectionToken, OnInit} from '@angular/core';
 
 import { Pay } from '../models/pay';
 import {CONFIG} from '../app.config';
@@ -11,11 +11,14 @@ const AppConfig = new InjectionToken('app.config');
   styleUrls: ['./pay-phone.component.css'],
   providers: [{provide: AppConfig, useValue: CONFIG }]
 })
-export class PayPhoneComponent {
-  pay: Pay = new Pay();
+export class PayPhoneComponent implements OnInit {
+  pay: Pay;
   msg: any;
 
   constructor (@Inject(AppConfig) config: any) {
     this.msg = config;
+  }
+  ngOnInit() {
+    this.pay = new Pay();
   }
 }
